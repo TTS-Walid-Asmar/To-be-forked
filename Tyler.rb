5d38d1ab45dfa2c997461a627fcbb80f5be61eeb
Tyler Seawell <tylerseawell4@gmail.com>
2016-04-19 15:47:12 -0400

    initial push

--- Tyler.rb
+++ Tyler.rb
@@ -1,1 +1,17 @@
 #Write a program that only accepts integers that are evenly divisible by 2. That is to say there will be no remainder.
+
+def divisible
+puts "Give me an integer that is divisible by 2!"
+integer = gets.chomp.to_i
+if integer == 0
+	puts "I can't divide by 0."
+	divisible
+elsif integer % 2 == 0
+	puts "I accept that!"
+else
+	puts "That number is not divisible by 2. I do not accept that!"
+	divisible
+end
+end
+
+divisible
