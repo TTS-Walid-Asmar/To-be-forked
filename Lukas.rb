fa5c59a7a42d2ee7640e5f68804639a993abd8ab
Lukas <Lukasbarry@yahoo.com>
2016-04-19 17:03:06 -0400

    completed ruby program

--- Lukas.rb
+++ Lukas.rb
@@ -1,1 +1,8 @@
 #Write a program that only accepts integers that are evenly divisible by 2. That is to say there will be no remainder.
+
+puts "Please enter a number"
+num1 = gets.chomp.to_i
+until (num1%2) == 0
+  puts "I'm sorry, it needs to be divisible by 2"
+  num1 = gets.chomp.to_i
+end
